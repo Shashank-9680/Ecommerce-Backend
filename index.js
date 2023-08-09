d63a70def1fe5347c4d31d87a452915bfe13c6f0
Shashank-9680 <shashankagrawal496@gmail.com>
2023-08-09 11:49:56 +0530

    order cart page added

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const { createProduct } = require("./controller/Product");
 const productRouter = require("./routes/Product");
 const categoriesRouter = require("./routes/Category");
 const brandsRouter = require("./routes/Brands");
+const usersRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Cart");
+const orderRouter = require("./routes/Order");
 const cors = require("cors");
 main().catch((err) => console.log(err));
 server.use(express.json());
@@ -16,6 +20,10 @@ server.use(
 server.use("/products", productRouter.router);
 server.use("/categories", categoriesRouter.router);
 server.use("/brands", brandsRouter.router);
+server.use("/users", usersRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", cartRouter.router);
+server.use("/orders", orderRouter.router);
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
